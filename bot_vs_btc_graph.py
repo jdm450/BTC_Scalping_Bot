d0d4783763b718afa717a27f798293f999114958
jdm450 <jackmahoney450@gmail.com>
2024-12-11 21:30:08 -0700

    created graph to view account compared to BTC price

--- bot_vs_btc_graph.py
+++ bot_vs_btc_graph.py
@@ -15,16 +15,27 @@ filtered_df = transactions_df[transactions_df['capital'] != 0]
 capital_df = filtered_df['capital']
 df = capital_df.drop(capital_df.index[0:26])
 
+# Adjust Data Frame for Bitcoin Price
+btc_price_df = pd.json_normalize(data, 'transactions')
+btc_price_df['price'] = btc_price_df['price'].astype(int)
+price_df = btc_price_df['price']
+
 # Add Percent Increase Column for Account Value
 df_pct_increase = ((df - df.iloc[0]) / df.iloc[0]) * 100
 
+# Add Percent Increase Column for BTC Price
+df_btc_pct = ((price_df - price_df.iloc[0]) / price_df.iloc[0]) * 100
+
 # Plot data
 plt.style.use('dark_background')
-ax = df_pct_increase.plot(kind='line', figsize=(14,8), color='red')
+
+ax = df_pct_increase.plot(kind='line', figsize=(14,8), label='Scalping Bot', color='red')
+plt.plot(df_btc_pct, label='BTC Price', color='gainsboro')
 plt.title('BTC Scalping Bot', fontsize=16)
 plt.xlabel('Starts: 12/10/2024 1:00 PM', fontsize=14)
 plt.ylabel('Percent Increase', fontsize=14)
 plt.grid(color='dimgray')
+plt.legend()
 
 plt.xticks([])
 
